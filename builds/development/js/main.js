ab20915da2bbd8d433a15cc6807ae3e455b5d150
jachep <garza.facil@gmail.com>
2016-07-19 10:59:46 -0500

    developer workflow set up

---
+++ builds/development/js/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by f.garza.cavazos on 7/19/2016.
+ */
+var cuatro = 'cuatro',
+    cinco = 5;
+    seis = null;
+
+function chida2 (){
+    console.log('hola mundo 2');
+}
+
+var uno = 'uno',
+    dos = 'dos',
+    tres = 3;
+
+
+function chida (){
+    console.log('Hola Mundo');
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
     less = require('gulp-less'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    cleancss = require('gulp-cleancss'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch');
 
 //Compile Jade
 
@@ -19,11 +24,45 @@ gulp.task('less', function () {
        .pipe(gulp.dest('builds/development/css'));
 });
 
-//Debbuging JsHint
+//Finds Bugs JsHint
 
 gulp.task('lint', function () {
    gulp.src('src/js/**/*.js')
        .pipe(jshint())
        .pipe(jshint.reporter('default'));
 });
+
+//Concat Minify JsFiles
+
+gulp.task('minjs', function () {
+   gulp.src('src/js/**/*.js')
+       .pipe(concat('main.js'))
+       .pipe(uglify())
+       .pipe(rename({suffix: '.min'}))
+       .pipe(gulp.dest('builds/production/js'));
+});
+
+//Concat Minify CSS
+
+gulp.task('mincss', function () {
+    gulp.src('builds/development/css/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(cleancss({keepBreaks: false}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('builds/production/css'));
+});
+
+//Watch Task
+
+gulp.task('watch', function () {
+    gulp.watch(['src/js/*.js'],['minjs']);
+    gulp.watch(['builds/development/css/*.css'],['mincss']);
+});
+
+
+
+
+
+
+
 
